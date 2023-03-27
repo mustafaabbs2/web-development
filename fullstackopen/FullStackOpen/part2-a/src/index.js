1a4c34be3f774c7bf57fd724f0728453b495c492
Mustafa Bhotvawala <mustafaabbs2@gmail.com>
2023-03-27 22:05:21 +0530

    changes

--- fullstackopen/FullStackOpen/part2-a/src/index.js
+++ fullstackopen/FullStackOpen/part2-a/src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom'
 import App from './App'
+import './index.css'
+
 
 const notes = [
   {
     id: 1,
-    content: 'HTML is easy',
+    content: 'HTML is easy Ali',
     date: '2019-05-30T17:30:31.098Z',
     important: true
   },
