50da610b212eba907cbc7729aea168a824bda087
Mustafa Bhotvawala <mustafaabbs2@gmail.com>
2023-05-07 12:22:05 +0530

    removed pwd

--- fullstackopen/part3/REST/mongo.js
+++ fullstackopen/part3/REST/mongo.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-// if (process.argv.length<3) {
-//   console.log('give password as argument')
-//   process.exit(1)
-// }
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
 
-// const password = process.argv[2]
+const password = process.argv[2]
 
 const url =
-  `mongodb+srv://mustafaabbs2:mustafa1217@cluster0.86ofurn.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://mustafaabbs2:${password}@cluster0.86ofurn.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
@@ -20,19 +20,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//   content: 'HTML is Easy',
-//   important: true,
-// })
+const note = new Note({
+  content: 'Mustafa is the best',
+  important: true,
+})
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+// Note.find({}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
